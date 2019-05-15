c20ded8c33798f3e465854a6520c043355beb76f
nikita <xstreletssx@gmail.com>
2019-05-15 15:00:30 +0300

    Update useLockBodyScroll.js

--- src/useLockBodyScroll.js
+++ src/useLockBodyScroll.js
@@ -1,10 +1,14 @@
+import { useLayoutEffect } from 'react';
+
 function useLockBodyScroll(condition) {
   useLayoutEffect(() => {
     const originalOverflow = window.getComputedStyle(document.body).overflow;
-    const overflow = condition ? "hidden" : originalOverflow;
+    const overflow = condition ? 'hidden' : originalOverflow;
     document.body.style.overflow = overflow;
     return () => {
       document.body.style.overflow = originalOverflow;
     };
   }, [condition]);
-}
+ }
+
+export default useLockBodyScroll;
